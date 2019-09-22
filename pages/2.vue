a2f2414eac356e0273858b7019a6186ec2acacd9
Diego <dvalenzuela3@uc.cl>
2019-09-21 23:25:34 -0300

    first commit

---
+++ pages/2.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<p>Hola from pagina 2</p>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<p>Hola from page</p>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
